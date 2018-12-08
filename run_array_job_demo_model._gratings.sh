190a038a2e27b1b2b12b391ac7ba5912c320f482
Xinzhu-Fang <xf15@hampshire.edu>
2018-12-08 14:18:15 -0500

    a copy of run_job for gratings

--- run_array_job_demo_model._gratings.sh
+++ run_array_job_demo_model._gratings.sh
@@ -6,28 +6,26 @@
 #SBATCH --mem=20G
 #SBATCH --gres=gpu:tesla-k80:1
 
-my_command1="python Cindy_demo.py --net res101 --dataset pascal_voc\
+my_command1="python Cindy_demo_gratings.py --net res101 --dataset pascal_voc\
                    --checksession 1 --checkepoch 7 --checkpoint 10021 \
-                   --cuda"
+                   --cuda --stimuli gratings"
 echo $my_command1
 eval $my_command1
 
-my_command2="python Cindy_demo.py --net vgg16 --dataset pascal_voc\
+my_command2="python Cindy_demo_gratings.py --net vgg16 --dataset pascal_voc\
                    --checksession 1 --checkepoch 1 --checkpoint 10021 \
-                   --cuda"
+                   --cuda --stimuli gratings"
 echo $my_command2
 eval $my_command2
 
-my_command3="python Cindy_demo.py --net res101 --dataset vg\
+my_command3="python Cindy_demo_gratings.py --net res101 --dataset vg\
                    --checksession 1 --checkepoch 20 --checkpoint 16193 \
-                   --cuda"
+                   --cuda --stimuli gratings"
 echo $my_command3
 eval $my_command3
 
-my_command4="python Cindy_demo.py --net vgg16 --dataset vg\
+my_command4="python Cindy_demo_gratings.py --net vgg16 --dataset vg\
                    --checksession 1 --checkepoch 19 --checkpoint 48611 \
-                   --cuda"
+                   --cuda --stimuli gratings"
 echo $my_command4
 eval $my_command4
-
-
