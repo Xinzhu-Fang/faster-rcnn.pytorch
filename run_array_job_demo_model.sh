5f28ee211af7d0828013bec27b1d0a7af2651fba
Xinzhu-Fang <xf15@hampshire.edu>
2018-12-08 12:07:06 -0500

    gitignore didn't take effect

--- run_array_job_demo_model.sh
+++ run_array_job_demo_model.sh
@@ -2,13 +2,31 @@
 #
 
 #SBATCH --ntasks=1
-#SBATCH --time=1:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem=20G
 #SBATCH --gres=gpu:tesla-k80:1
 
-my_command="python demo.py --net vgg16 --dataset pascal_voc\
+my_command1="python Cindy_demo.py --net res101 --dataset pascal_voc\
+                   --checksession 1 --checkepoch 7 --checkpoint 10021 \
+                   --cuda"
+echo $my_command1
+eval $my_command1
+
+my_command2="python Cindy_demo.py --net vgg16 --dataset pascal_voc\
                    --checksession 1 --checkepoch 1 --checkpoint 10021 \
-                   --cuda --load_dir models > my_demo.txt"
-echo $my_command
-eval $my_command
+                   --cuda"
+echo $my_command2
+eval $my_command2
+
+my_command3="python Cindy_demo.py --net res101 --dataset vg\
+                   --checksession 1 --checkepoch 20 --checkpoint 16193 \
+                   --cuda"
+echo $my_command3
+eval $my_command3
+
+my_command4="python Cindy_demo.py --net vgg16 --dataset vg\
+                   --checksession 1 --checkepoch 19 --checkpoint 48611 \
+                   --cuda"
+echo $my_command4
+eval $my_command4
 
